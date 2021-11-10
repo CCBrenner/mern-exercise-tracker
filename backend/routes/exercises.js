317826d7a1edcf08b49687e570d6ed91f47e7fb8
Collyn Christopher Brenner <collynbrenner@gmail.com>
2021-11-10 12:49:24 -0600

    Added three new routes to exercises

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -30,4 +30,34 @@ router.route('/add').post((req, res) => {
      .catch(err => res.status(400).json('Error: ' + err));
 });
 
+// route that GETs exercise data by an ID parameter (ID that was assigned automatically by MongoDB)
+router.route('/:id').get((req, res) => {
+    Exercise.findById(req.params.id)
+     .then(exercises => res.json(exercises))
+     .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// route that finds exercise by ID and deletes it
+router.route('/:id').delete((req, res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+     .then(() => res.json('Exertcise deleted.'))
+     .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// route that finds and updates exercise by ID using a POST request
+router.route('/update/:id').post((req, res) => {
+    Exercise.findByIdAndUpdate(req.params.id)
+     .then(exercise => {
+        exercise.username = req.body.username;
+        exercise.description = req.body.description;
+        exercise.duration = Number(req.body.duration);
+        exercise.date = Date.parse(req.body.date);
+
+        exercise.save()
+         .then(() => res.json('Exercise updated!'))
+         .catch(err => res.status(400).json('Error: ' + err));
+     })
+     .catch(err => res.status(400).json('Error: ' + err));
+});
+
 module.exports = router;  // export the router for Express to use
